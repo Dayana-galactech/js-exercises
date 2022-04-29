e1415fa11ecf9f371ff83c3c0c71a05db341bc44
Ayman Hamdoun <ayman.s.hamdoun@gmail.com>
2022-04-29 13:10:38 +0300

    fix indents

--- api.js
+++ api.js
@@ -1,8 +1,9 @@
 const randomUserGenerator=()=>{
     fetch('https://randomuser.me/api').then((response) => {
-        return response.json()}).then((data)=>{
-            console.log(data)
-        })
+        return response.json()
+    }).then((data)=>{
+        console.log(data)
+    })
 }
 
 window.onload=()=>{
